b751454069c8c60031a343c56418981fe1f24951
노석현 <shtjrgus010@github.com>
2024-10-30 21:22:26 +0900

    setting

---
+++ backend/houses/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import House
+# Register your models here.
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    list_display=[
+    "name",
+    "price",
+    "description",
+    "address",
+    ]
