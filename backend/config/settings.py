6e87e5214822858dd46b8934e861da1bcdf54a26
노석현 <shtjrgus010@github.com>
2024-11-07 12:20:30 +0900

    feat: Django REST Framework API VIews and Serializers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
     "users.apps.UsersConfig",
@@ -49,7 +52,7 @@ SYSTEM_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
